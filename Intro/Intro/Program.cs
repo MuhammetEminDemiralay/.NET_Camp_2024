e039f00198f27fcb6f3f456e54765140f087740e
Muhammet Emin Demiralay <pepmuhammet.42@gmail.com>
2024-02-04 23:22:22 +0300

    Add project files.

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+
+CourseManager courseManager = new();
+Course[] courses = courseManager.GetAll();
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " - " + courses[i].Price + " - " + courses[i].Description);
+}
